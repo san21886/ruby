dcb735ccf88515f0d2e66314b553e9643e29a6d7
Santosh Kumar <sk21886-mygit@yahoo.co.in>
2012-09-18 18:47:52 +0530

    modified to to, add index page, bulk deltion capability, inline links to perform all operations

--- file_editing_sinatra_app.rb
+++ file_editing_sinatra_app.rb
@@ -2,11 +2,13 @@ require 'sinatra'
 
 
 #authentication source:http://ididitmyway.heroku.com/past/2011/2/22/really_simple_authentication_in_sinatra/
+
 set :username,'admin'
 set :token,'ankjoncnDhbsan'
-set :password,'admin'
+set :password,'me'
 
 dir=File.dirname(File.expand_path(__FILE__))+"/"+"data"
+previous_page="/"
 
 helpers do
 	def delete? ; request.cookies[settings.username] == settings.token ; end
@@ -18,14 +20,54 @@ helpers do
        	end
 end
 
-get '/list' do
-	files=EditFile.new.get_dir_files(dir)
-	files.join("\r\n")
+get '/' do
+	erb :fe_index
 end
 
-get '/create/*' do
-	file=File.join(dir, params[:splat])
-	EditFile.new.create_file(file)
+get '/list' do
+	files=EditFile.new.get_dir_files(dir)
+	@files=files.join("<br/>")
+	erb :fe_index
+end
+
+get '/edit' do
+	files=EditFile.new.get_dir_files(dir)
+	@files=files.map{|val| fn=File.basename(val); "<a href='/edit/#{fn}'>#{val}</a>"}.join("<br/>")
+	erb :fe_index
+end
+
+get '/delete' do
+	previous_page="/delete"
+	protected!
+	files=EditFile.new.get_dir_files(dir)
+	@files=files.map{|val| fn=File.basename(val); "<form method='post'><input type='checkbox' name=#{fn}>#{val}</input>"}.join("<br/>")
+	@files+="<br/><button type='submit'>Submit</button></form>"
+	erb :fe_index
+end
+
+post '/delete' do
+	files=params.select{|key,val| val=="on"}.keys
+	files.map!{|file| File.join(dir, file)}
+	EditFile.new.delete_file(files)
+	redirect '/'
+end
+
+get '/create' do
+	erb :create_file
+end
+
+post '/create' do
+	if not params["fn"] or params["fn"].chomp.strip.empty?
+		@content=params["content"]
+		erb :create_file
+	else
+		file_creation_status = EditFile.new.create_file(File.join(dir,params["fn"].chomp.strip), params["content"])
+		if file_creation_status=="ok"
+			redirect "/"
+		else
+			file_creation_status
+		end
+	end
 end
 
 get '/edit/*' do
@@ -40,12 +82,7 @@ post '/edit/*' do
 	file=File.join(dir, params[:splat])
 	text=params[:ta1]
 	EditFile.new.modify_file_content(file, text)
-end
-
-get '/delete/*' do
-	protected!
-	file=File.join(dir, params[:splat])
-	EditFile.new.delete_file(file)
+	redirect '/'
 end
 
 get '/login' do
@@ -54,11 +91,13 @@ end
 
 get '/logout' do
 	response.set_cookie(settings.username, false) 
+	redirect '/'
 end
 
 post '/login' do
 	if params['username']==settings.username&&params['password']==settings.password
 		response.set_cookie(settings.username,settings.token)
+		redirect previous_page
 	else
 		"Username or Password incorrect"
 	end
@@ -73,9 +112,11 @@ class EditFile
 		return files
 	end
 
-	def delete_file(file)
-		if File.exists?(file)
-			File.delete(file)
+	def delete_file(files)
+		files.each do |file|
+			if File.exists?(file)
+				File.delete(file)
+			end
 		end
 	end
 
@@ -90,7 +131,7 @@ class EditFile
 					file << content
 				end
 			end
-			return "#{file} created"
+			return "ok"
 		end
 	end
 
