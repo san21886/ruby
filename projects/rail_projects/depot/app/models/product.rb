384474a3561ef53ba2e7291fa6b3d5ff662381ab
Santosh Kumar <sk21886-mygit@yahoo.co.in>
2012-10-14 16:05:30 +0530

    validation and tests added

--- projects/rail_projects/depot/app/models/product.rb
+++ projects/rail_projects/depot/app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates :description, :image_url, :title, :presence => true #description, title, image to be non empty string
+  validates :title, :uniqueness => true #title to be uniq
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01} #price to be numeric value greater than 0
+  #format option, matches a field against a regular expression
+  validates :image_url, :format => {
+	  :with => %r{\.(gif|jpg|png)$}i,
+	  :message => 'must be a URL for GIF, JPG or PNG image.'}
 end
--- projects/rail_projects/depot/test/unit/product_test.rb
+++ projects/rail_projects/depot/test/unit/product_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	fixtures :products
+	#The fixtures directive loads the fixture data corresponding to the given model
+	#name into the corresponding database table before each test method in the
+	#test case is run. The name of the fixture file determines the table that is loaded,
+	#so using :products will cause the products.yml fixture file to be used.
+	#
+	#fixtures directive means that the products table will be emptied out and then
+	#populated with the rows defined in the fixture before each test method
+	#is run.
+	#
+	#default for tests is to load all fixtures before running the test
+	#
+	#For each fixture it loads into a
+	#test, Rails defines a method with the same name as the fixture. You can use
+	#this method to access preloaded model objects containing the fixture data:
+	#simply pass it the name of the row as defined in the YAML fixture file, and it’ll
+	#return a model object containing that row’s data
+	#
+	#products(:ruby) will return the row object
+	#to run unit test: rake test:units
+
+	test "product is not valid without a unique title" do
+
+		product = Product.new(:title => products(:ruby).title,
+				      :description => "yyy" ,
+				      :price => 1,
+				      :image_url => "fred.gif" )
+		assert !product.save
+		assert_equal "has already been taken" , product.errors[:title].join('; ' )
+	end
+  
 end
